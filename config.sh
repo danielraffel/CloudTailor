8222ba89e30732828e651bf9d769837f8a71d904
daniel raffel <daniel.raffel@gmail.com>
2024-01-11 20:53:00 -0800

    Update config.sh
    fixed generating variables.txt and generating the missing prompts (os and machine type)

--- config.sh
+++ config.sh
@@ -27,6 +27,98 @@ load_variables() {
     fi
 }
 
+# Function to prompt user input for a variable, with an option to use a default value.
+ask_and_set() {
+    local var_name=$1
+    local message=$2
+    local default_value=$3
+    local confirmation
+    local input
+
+    while true; do
+        echo "$message"
+        read -p "Enter value ($default_value): " input
+        input=${input:-$default_value}
+        echo "You entered: $input"
+        read -p "Is this correct? (y/n): " confirmation
+        if [[ $confirmation == "y" ]]; then
+            eval "$var_name='$input'"
+            break
+        fi
+    done
+}
+
+# Function to prompt for Google Cloud region
+prompt_for_region() {
+    local confirmed="n"
+    while [[ $confirmed != "y" ]]; do
+        echo "\nPlease select the Google Cloud region for your VM:"
+        echo " 1) Oregon: us-west1"
+        echo " 2) Iowa: us-central1"
+        echo " 3) South Carolina: us-east1"
+        read -p "Select a region (1/2/3): " region_choice
+
+        case "$region_choice" in
+            1) REGION="us-west1" ;;
+            2) REGION="us-central1" ;;
+            3) REGION="us-east1" ;;
+            *) echo "Invalid choice. Please select a valid option."; continue ;;
+        esac
+
+        read -p "You selected: $REGION. Is this correct? (y/n): " confirmed
+    done
+}
+
+# Function to prompt for GCP server type
+prompt_for_server_type() {
+    local server_choice
+    local server_confirmed="n"
+    while [[ $server_confirmed != "y" ]]; do
+        echo "\nSelect the type of GCP server:"
+        echo " 1) e2-micro (FREE)"
+        echo " 2) e2-small"
+        echo " 3) e2-medium"
+        echo " 4) custom (enter a custom server type)"
+        read -p "Select an option (1/2/3/4): " server_choice
+
+        case "$server_choice" in
+            1) SERVER_TYPE="e2-micro" ;;
+            2) SERVER_TYPE="e2-small" ;;
+            3) SERVER_TYPE="e2-medium" ;;
+            4) read -p "Enter custom server type: " SERVER_TYPE ;;
+            *) echo "Invalid choice. Please select a valid option."; continue ;;
+        esac
+
+        read -p "You selected: $SERVER_TYPE. Is this correct? (y/n): " server_confirmed
+    done
+}
+
+# Function to prompt for OS selection
+prompt_for_os() {
+    local os_choice
+    local os_confirmed="n"
+    while [[ $os_confirmed != "y" ]]; do
+        echo "\nSelect the OS for your server:"
+        echo " 1) ubuntu-os-cloud/ubuntu-2204-lts"
+        echo " 2) ubuntu-os-cloud/ubuntu-2004-lts"
+        echo " 3) cos-cloud/cos-arm64-109-lts"
+        echo " 4) centos-cloud/centos-stream-9"
+        echo " 5) other (enter Image project/Image family)"
+        read -p "Select an option (1/2/3/4/5): " os_choice
+
+        case "$os_choice" in
+            1) OS_TYPE="ubuntu-os-cloud/ubuntu-2204-lts" ;;
+            2) OS_TYPE="ubuntu-2004-lts" ;;
+            3) OS_TYPE="cos-cloud/cos-arm64-109-lts" ;;
+            4) OS_TYPE="centos-cloud/centos-stream-9" ;;
+            5) read -p "Enter other OS image name Image project/Image family: " OS_TYPE ;;
+            *) echo "Invalid choice. Please select a valid option."; continue ;;
+        esac
+
+        read -p "You selected: $OS_TYPE. Is this correct? (y/n): " os_confirmed
+    done
+}
+
 # Prompts the user to continue with the script execution.
 echo "This script automates the deployment of a Google Cloud VM with the Docker images of your choice.\nA Python script generates a Terraform file and other scripts.\nOpenAI is used to generate the Docker YAML file based on the software you choose to install."
 read -p "Do you want to proceed? (y/n): " proceed
@@ -45,118 +137,36 @@ fi
 if ! load_variables; then
     echo "Setting up configuration..."
 
-    # Function to prompt user input for a variable, with an option to use a default value.
-    ask_and_set() {
-        local var_name=$1
-        local message=$2
-        local default_value=$3
-        local confirmation
-        local input
-
-        while true; do
-            echo "$message"
-            read -p "Enter value ($default_value): " input
-            input=${input:-$default_value}
-            echo "You entered: $input"
-            read -p "Is this correct? (y/n): " confirmation
-            if [[ $confirmation == "y" ]]; then
-                sed -i '' "s/^$var_name = .*/$var_name = \"$input\"/" setup.py
-                break
-            fi
-        done
-    }
-
-    # Interactive functions to set up various configuration parameters.
-    # Function to prompt for Google Cloud region
-    prompt_for_region() {
-        local confirmed="n"
-        while [[ $confirmed != "y" ]]; do
-            echo "\nPlease select the Google Cloud region for your VM:"
-            echo " 1) Oregon: us-west1"
-            echo " 2) Iowa: us-central1"
-            echo " 3) South Carolina: us-east1"
-            read -p "You selected: $REGION. Is this correct? (y/n): " confirmed
-        done
-        sed -i '' "s/^region = .*/region = \"$REGION\"/" setup.py
-    }
-
-    # Function to prompt for GCP server type
-    prompt_for_server_type() {
-        local server_choice
-        local server_confirmed="n"
-        while [[ $server_confirmed != "y" ]]; do
-            echo "\nSelect the type of GCP server:"
-            echo " 1) e2-micro (FREE)"
-            echo " 2) e2-small"
-            echo " 3) e2-medium"
-            echo " 4) custom (enter a custom server type)"
-            read -p "Select an option (1/2/3/4): " server_choice
-
-            case "$server_choice" in
-                1) SERVER_TYPE="e2-micro" ;;
-                2) SERVER_TYPE="e2-small" ;;
-                3) SERVER_TYPE="e2-medium" ;;
-                4) read -p "Enter custom server type: " SERVER_TYPE ;;
-                *) echo "Invalid choice. Please select a valid option."; continue ;;
-            esac
-
-            read -p "You selected: $SERVER_TYPE. Is this correct? (y/n): " server_confirmed
-        done
-        sed -i '' "s/^server_type = .*/server_type = \"$SERVER_TYPE\"/" setup.py
-    }
-
-    # Function to prompt for OS selection
-    prompt_for_os() {
-        local os_choice
-        local os_confirmed="n"
-        while [[ $os_confirmed != "y" ]]; do
-            echo "\nSelect the OS for your server:"
-            echo " 1) ubuntu-os-cloud/ubuntu-2204-lts"
-            echo " 2) ubuntu-os-cloud/ubuntu-2004-lts"
-            echo " 3) cos-cloud/cos-arm64-109-lts"
-            echo " 4) centos-cloud/centos-stream-9"
-            echo " 5) other (enter Image project/Image family)"
-            read -p "Select an option (1/2/3/4/5): " os_choice
-
-            case "$os_choice" in
-                1) OS_TYPE="ubuntu-os-cloud/ubuntu-2204-lts" ;;
-                2) OS_TYPE="ubuntu-2004-lts" ;;
-                3) OS_TYPE="cos-cloud/cos-arm64-109-lts" ;;
-                4) OS_TYPE="centos-cloud/centos-stream-9" ;;
-                5) read -p "Enter other OS image name Image project/Image family: " OS_TYPE ;;
-                *) echo "Invalid choice. Please select a valid option."; continue ;;
-            esac
-
-            read -p "You selected: $OS_TYPE. Is this correct? (y/n): " os_confirmed
-        done
-        sed -i '' "s/^os_type = .*/os_type = \"$OS_TYPE\"/" setup.py
-    }
-
     # Set app_hostname variable
     ask_and_set app_hostname "Enter your domain (e.g., YOUR.DOMAIN.COM):" "YOUR.DOMAIN.COM"
-
-    # Set SSH key paths
-    echo "Please enter the path to your SSH public key (e.g., ~/.ssh/gcp.pub):"
-    read -p "Path: " ssh_public_key_path
-    ssh_public_key_path=${ssh_public_key_path:-~/.ssh/gcp.pub}
-    sed -i '' "s|^ssh_public_key_path = .*|ssh_public_key_path = os.path.expanduser(\"$ssh_public_key_path\")|" setup.py
-
-    # Docker images configuration
-    echo "Enter Docker images to install you can search for packages at https://hub.docker.com (separate multiple packages by space, e.g., tiangolo/uvicorn-gunicorn-fastapi:python3.11 n8nio/n8n):"
-    read -p "Docker images: " docker_images
-    docker_images_array=($docker_images)
-    docker_images_formatted=$(printf ", \"%s\"" "${docker_images_array[@]}")
-    docker_images_formatted="[${docker_images_formatted:2}]"
-    sed -i '' "s|^docker_images = .*|docker_images = $docker_images_formatted|" setup.py
-
-    # OpenAI API key configuration
-    echo "Please enter your OpenAI API key:"
-    read -p "OpenAI API Key: " OPENAI_API_KEY
-    sed -i '' "s|^OPENAI_API_KEY = .*|OPENAI_API_KEY = \"$OPENAI_API_KEY\"|" setup.py
 
     # Prompt for Google Cloud region
     prompt_for_region
 
+    # Prompt for GCP server type
+    prompt_for_server_type
+
+    # Prompt for OS selection
+    prompt_for_os
+
+    # Set SSH key paths
+    ask_and_set ssh_public_key_path "Please enter the path to your SSH public key:" "for example: ~/.ssh/gcp.pub"
+
+    # Docker images configuration
+    ask_and_set docker_images "Enter Docker images to install (separate multiple with a space: teslamate/teslamate:latest n8nio/n8n):" ""
+
+    # OpenAI API key configuration
+    ask_and_set OPENAI_API_KEY "Please enter your OpenAI API key:" ""
+
+    # Generate variables.txt file
+    echo "Creating variables.txt with the configuration..."
+    echo "app_hostname=\"$app_hostname\"" > variables.txt
+    echo "docker_images=\"$docker_images\"" >> variables.txt
+    echo "region=\"$REGION\"" >> variables.txt
+    echo "os_type=\"$OS_TYPE\"" >> variables.txt
+    echo "server_type=\"$SERVER_TYPE\"" >> variables.txt
+    echo "ssh_public_key_path=\"$ssh_public_key_path\"" >> variables.txt
+    echo "OPENAI_API_KEY=\"$OPENAI_API_KEY\"" >> variables.txt
 fi
 
 # Run the Python script
